51a4d093521eadf86c86706219dffe60a7f43853
DanViTrinh <danvitrinh@outlook.com>
2024-03-18 16:13:05 +0100

    script to train pipeline

--- mobai_svm/svm_train.sh
+++ mobai_svm/svm_train.sh
@@ -1,7 +1,8 @@
 bonaFideFeatures="../datasets/unibo/runnable/Feature_Bonafide/"
-morphedAttackFeatures="../datasets/mordiff/runnable/Feature_Morphed/"
+# morphedAttackFeatures="../datasets/mordiff/full_morphed_features/Feature_Morphed/"
+morphedAttackFeatures="../datasets/mordiff/runnable3/Feature_Morphed/"
 
-modelOutput="../models/mordiff_morph_2/"
+modelOutput="../models/mordiff_morph_original_w_unibo_test/
 
 python3 svm_training_pipeline.py --bonaFideFeatures $bonaFideFeatures \
 	--morphedAttackFeatures $morphedAttackFeatures \
