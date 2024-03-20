2b332f549fdf5e48436ccfd05581c63fe8171b10
DanVi007 <danvitrinh@outlook.com>
2024-03-20 11:39:12 +0100

    moved shell scripts to ignored folder

--- mobai_dataset_utils/create_dataset.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/sh
-
-originalDataset="datasets/unibo/runnable"
-newDataset="datasets/mordiff/full_morphed_features"
-
-outputDataset="datasets/mordiff/runnable3"
-
-python create_dataset.py --original $originalDataset --new $newDataset --output $outputDataset
