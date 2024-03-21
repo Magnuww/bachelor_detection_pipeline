bfef80fddf32da74b7b0951f8cff4b3743f56bef
DanVi007 <danvitrinh@outlook.com>
2024-03-21 09:09:32 +0100

    moved run scripts to ignored folder

--- mobai_svm/svm_test.sh
+++
@@ -1,8 +0,0 @@
-bonaFideFeatures="../datasets/unibo_w_mordiff_test/runnable/Feature_Bonafide/"
-morphedAttackFeatures="../datasets/unibo_w_mordiff_test/runnable/Feature_Morphed/"
-
-modelOutput="../models/unibo_morph_w_mordiff_test/PythonSVM"
-
-python3 svm_testing_pipeline.py --bonaFideFeatures $bonaFideFeatures \
-	--morphedAttackFeatures $morphedAttackFeatures \
-	--modelOutput $modelOutput
--- mobai_svm/svm_train.sh
+++
@@ -1,9 +0,0 @@
-bonaFideFeatures="../datasets/unibo/runnable/Feature_Bonafide/"
-# morphedAttackFeatures="../datasets/mordiff/full_morphed_features/Feature_Morphed/"
-morphedAttackFeatures="../datasets/mordiff/runnable3/Feature_Morphed/"
-
-modelOutput="../models/mordiff_morph_original_w_unibo_test/
-
-python3 svm_training_pipeline.py --bonaFideFeatures $bonaFideFeatures \
-	--morphedAttackFeatures $morphedAttackFeatures \
-	--modelOutput $modelOutput
