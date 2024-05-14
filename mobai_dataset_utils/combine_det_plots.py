03b7030ce733e63bc4eb1a257cd5fc0e8ad6cda6
DanViTrinh <danvitrinh@outlook.com>
2024-05-14 15:50:58 +0200

    lint det combining det plots

--- mobai_dataset_utils/combine_det_plots.py
+++ mobai_dataset_utils/combine_det_plots.py
@@ -39,35 +39,38 @@ def plot_det(det_plot_vars, fig, axs, model_name):
 if __name__ == "__main__":
     parser = ArgumentParser()
     parser.add_argument(
-        "--resultPaths",
+        "--resultsPath",
         help="The results that will be combined",
         required=True,
     )
-    # parser.add_argument(
-    #     "--output", type=str, help="Path to the result outputs", required=True
-    # )
 
     args = parser.parse_args()
 
-    resultFolders = os.listdir(args.resultPaths)
+    resultFolders = os.listdir(args.resultsPath)
 
     fig, axs = plt.subplots(1, 1, figsize=(10, 10))
 
+    figName = ""
+
     for folder in resultFolders:
         resultPath = os.path.join(
-            args.resultPaths,
+            args.resultsPath,
             folder,
-            "concatenate",
-            "model_-s 0 -t 0 -c 10 -b 1 -q_os_True",
+            # "concatenate",
+            # "model_-s 0 -t 0 -c 10 -b 1 -q_os_True",
             PLOT_VAR_FILE_NAME,
         )
         with open(resultPath, "rb") as file:
             det_plot_vars = pickle.load(file)
             fig, axs = plot_det(det_plot_vars, fig, axs, folder)
+        if figName != "":
+            figName += "+"
+
+        figName += folder
 
     x = np.logspace(-3, 0, 10)
     axs.loglog(x, x, ls="--")
     axs.legend()
     axs.set_title("plot_apcr_bpcr")
-    figName = "combined_plots.png"
-    fig.savefig(figName)
+    figName += ".png"
+    fig.savefig(os.path.join(args.resultsPath, figName))
